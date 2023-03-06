ee71e2df4cfbef18c307923d68b8ba396c86ff9d
Amee Madhani <e21cseu0171@bennett.edu.in>
2023-03-06 12:06:00 +0530

    react is funn done till props

---
+++ React/import export modules/src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+// import pi from "./math"
+import pie, {doublePi, triplePi} from "./math"
+
+
+ReactDOM.render(
+  <ul>
+    <li>{pie}</li>
+    <li>{doublePi()}</li>
+    <li>{triplePi()}</li>
+  </ul>,
+  document.getElementById("root")
+);
